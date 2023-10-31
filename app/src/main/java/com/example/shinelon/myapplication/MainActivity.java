b9a342c10cd0e445445971178217bdddd02a6d39
zhangyd <zhangyd@yuntian.com>
2023-10-31 18:09:56 +0800

    提交

--- app/src/main/java/com/example/shinelon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shinelon/myapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.shinelon.myapplication;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,10 +40,61 @@ import com.hjq.permissions.Permission;
 import com.hjq.permissions.XXPermissions;
 import com.tencent.smtt.sdk.QbSdk;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.Semaphore;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private String cameraId;
+    private CameraCaptureSession cameraCaptureSessions;
+    private CameraDevice cameraDevice;
+    private CaptureRequest captureRequest;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private MediaRecorder mediaRecorder;
+    private boolean isRecording = false;
+
+    private TextureView textureView;
+    private Button recordButton;
+
+    private File videoFile;
+    private HandlerThread backgroundThread;
+    private Handler backgroundHandler;
+    private Semaphore cameraOpenCloseLock = new Semaphore(1);
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+//            penCamera();
+
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            // 在这里关闭相机和释放资源
+            closeCamera();
+            return true;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+        // 其他 SurfaceTexture 回调方法
+    };
 
 
     @Override
@@ -30,45 +104,20 @@ public class MainActivity extends AppCompatActivity {
 
         requestPermissions();
 
-        TextView textView = findViewById(R.id.hello);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,FileOpenActivity.class);
-                startActivity(intent);
-            }
-        });
+        testView();
 
-    }
-
-    private void initX5Core() {
-        QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
-
-            @Override
-            public void onViewInitFinished(boolean arg0) {
-                // TODO Auto-generated method stub
-                //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-                Log.d("app", " onViewInitFinished is " + arg0);
-            }
-
-            @Override
-            public void onCoreInitFinished() {
-                // TODO Auto-generated method stub
-            }
-        };
-        //x5内核初始化接口
-        QbSdk.initX5Environment(getApplicationContext(),  cb);
     }
 
 
     @SuppressLint("CheckResult")
     private void requestPermissions() {
         XXPermissions.with(this)
-                .permission(Permission.CAMERA)
+                .permission(Permission.CAMERA,Permission.WRITE_EXTERNAL_STORAGE)
                 .request(new OnPermissionCallback() {
                     @Override
                     public void onGranted(@NonNull List<String> list, boolean b) {
                         Toast.makeText(MainActivity.this,".....",Toast.LENGTH_SHORT);
+
                     }
 
                     @Override
@@ -79,5 +128,230 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    private void testView(){
+        textureView = findViewById(R.id.texture_view);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(textureListener);
+        recordButton = findViewById(R.id.button);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("xxxxxxx", "onClick: ");
+                if (isRecording) {
+                    stopRecording();
+                } else {
+                    checkPermission();
+                }
+            }
+        });
+    }
+
+    private void stopRecording() {
+        if (!isRecording) {
+            return;
+        }
+
+        // 停止录制
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+        mediaRecorder = null;
+
+        // 停止相机捕获会话
+        if (cameraCaptureSessions != null) {
+            cameraCaptureSessions.close();
+            cameraCaptureSessions = null;
+        }
+
+        // 更新录制状态
+        isRecording = false;
+        recordButton.setText("Start Recording");
+
+        // 释放相机设备
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+
+        // 最后，你可能需要处理视频保存或其他相关操作，例如，你可以将视频文件保存到设备的存储位置。
+    }
+
+
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, REQUEST_CAMERA_PERMISSION);
+        } else {
+            startRecording();
+        }
+    }
+
+    // 处理录制开始
+    private void startRecording() {
+        closeCamera();
+        openCamera();
+
+        try {
+            setUpMediaRecorder();
+            SurfaceTexture texture = textureView.getSurfaceTexture();
+            assert texture != null;
+            texture.setDefaultBufferSize(640, 480);
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+            List<Surface> surfaces = new ArrayList<>();
+
+            // 设置预览输出
+            Surface previewSurface = new Surface(texture);
+            surfaces.add(previewSurface);
+            captureRequestBuilder.addTarget(previewSurface);
+
+            // 设置录制输出
+            Surface recorderSurface = mediaRecorder.getSurface();
+            surfaces.add(recorderSurface);
+            captureRequestBuilder.addTarget(recorderSurface);
+
+            // 创建捕获会话
+            cameraDevice.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    cameraCaptureSessions = cameraCaptureSession;
+                    updatePreview();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            isRecording = true;
+                            recordButton.setText("Stop Recording");
+                            mediaRecorder.start();
+                        }
+                    });
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+
+                }
+                // 其他回调方法
+            }, backgroundHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // 定义 stateCallback
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraOpenCloseLock.release();
+            cameraDevice = camera;
+//            startRecording();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraOpenCloseLock.release();
+            camera.close();
+            cameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            cameraOpenCloseLock.release();
+            camera.close();
+            cameraDevice = null;
+            // 处理错误
+        }
+    };
+
+    // 设置MediaRecorder参数
+    private void setUpMediaRecorder() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mediaRecorder.setVideoSize(640, 480);
+        mediaRecorder.setVideoFrameRate(30);
+        mediaRecorder.setOutputFile(getVideoFilePath());
+        mediaRecorder.setVideoEncodingBitRate(10000000); // 10 Mbps
+        mediaRecorder.setVideoFrameRate(30);
+        mediaRecorder.setVideoSize(640, 480);
+        mediaRecorder.setPreviewDisplay(new Surface(textureView.getSurfaceTexture()));
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 获取视频文件路径
+    private String getVideoFilePath() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String videoFileName = "VIDEO_" + timeStamp + ".mp4";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+        videoFile = new File(storageDir, videoFileName);
+        return videoFile.getAbsolutePath();
+    }
+
+    private void closeCamera() {
+        try {
+            cameraOpenCloseLock.acquire();
+            if (cameraDevice != null) {
+                cameraDevice.close();
+                cameraDevice = null;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock the camera.");
+        } finally {
+            cameraOpenCloseLock.release();
+        }
+    }
+
+
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = manager.getCameraIdList()[0]; // 获取第一个相机设备
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            if (map == null) {
+                throw new RuntimeException("Cannot get available preview/video sizes.");
+            }
+
+            // 打开相机
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    manager.openCamera(cameraId, stateCallback, backgroundHandler);
+            } else {
+                requestCameraPermission();
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updatePreview() {
+        if (cameraDevice == null) {
+            return;
+        }
+
+        try {
+            captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 请求相机权限
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            // 解释为什么需要相机权限
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        }
+    }
 
 }
